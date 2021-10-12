253e22f123a5b8eca6cd2691e20197c6dd232e30
alohaia <liu.qihuan@outlook.com>
2021-10-12 10:17:59 +0800

    2021.10.12 更新主题

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if [[ $# -gt 0 ]]; then
+    mes="`date +%Y.%m.%d` $1"
+else
+    mes="`date +%Y.%m.%d`"
+fi
+
 git add .
-git commit -m `date +%Y.%m.%d`
+git commit -m "${mes}"
 git push
